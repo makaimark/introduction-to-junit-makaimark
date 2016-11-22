62ba98b79e98d2056681c2fc65df4185de273f45
makaimark <makaimark@gmail.com>
2016-11-22 14:42:25 +0100

    working version

--- src/Project/FileReaderMock.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by makaimark on 2016.11.21..
- */
-public class FileReaderMock implements FileReaderInterface{
-
-    public String readLines() {
-        return "a test this is a test line too";
-    }
-}
