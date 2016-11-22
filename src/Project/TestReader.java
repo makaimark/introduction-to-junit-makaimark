3a580be3179dfa4cda71f42293a2236f7a09445f
makaimark <makaimark@gmail.com>
2016-11-22 14:05:54 +0100

    merge

--- src/Project/TestReader.java
+++ src/Project/TestReader.java
@@ -1,9 +1,7 @@
-package Project;
-
 /**
  * Created by makaimark on 2016.11.21..
  */
-public class TestReader {
+public class FileReaderMock implements FileReaderInterface{
 
     public static String readLines() {
         return "a test this is a test line too";
